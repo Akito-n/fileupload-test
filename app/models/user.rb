130ddef724f352f18ba3672c42bbf43d870a9f2b
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-31 14:23:17 +0900

    複数アタッチできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,5 +33,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
-  has_one_attached :avatar
+  has_many_attached :avatar
 end
