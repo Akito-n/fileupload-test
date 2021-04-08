7e9508df8ecf86aaf8b3c7074c1b7f71e421f9eb
Akito-n <a.nagai@mof-mof.co.jp>
2021-04-08 14:38:51 +0900

    検証用にfileをアップロードしないものを用意した

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :edit_user, mutation: Mutations::EditUser
     field :update_user, mutation: Mutations::UpdateUser
+    field :update_user_only_name, mutation: Mutations::UpdateUserOnlyName
     field :say, mutation: Mutations::Say
   end
 end
